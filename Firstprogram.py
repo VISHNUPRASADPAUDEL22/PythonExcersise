90111fd69cd1880804e5cf56e0e588463fced07a
VISHNU PAUDEL <vishnupaudel1989@gmail.com>
2025-06-11 08:50:54 +0545

    About For

--- Firstprogram.py
+++ Firstprogram.py
@@ -1,8 +1,9 @@
-while True:
+for i in range ( 2,5):
+
     try:
         age = int(input("enter your age "))
         if age<15:
-                    print(f"(age) is miner")
+                print(f"(age) is miner")
         elif age>=15:
                     print(f"(age) is Old")
         else:
@@ -10,5 +11,4 @@ while True:
     except:
         print("please give number")
     run=input("dp you want to contiune??(Y/N)\n")
-    if run=="n":
-        break
+    
