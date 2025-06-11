2fa684f98531cc82bc80650148c7a8ebcbc0327f
VISHNU PAUDEL <vishnupaudel1989@gmail.com>
2025-06-11 08:27:35 +0545

    if else elif

--- Firstprogram.py
+++ Firstprogram.py
@@ -1,6 +1,11 @@
 
 try:
     age = int(input("enter your age "))#Exception
-    print(age)
+    if age<15:
+        print(f"(age) is miner")
+    elif age>=15:
+        print(f"(age) is Old")
+    else:
+        print("Enter a proper age")
 except:
     print("please give number")
