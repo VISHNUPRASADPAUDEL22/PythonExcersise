5911023b6439a9edead45a67fbf88145bfe130ba
VISHNU PAUDEL <vishnupaudel1989@gmail.com>
2025-06-11 08:45:53 +0545

    About While Loop

--- Firstprogram.py
+++ Firstprogram.py
@@ -1,11 +1,14 @@
-
-try:
-    age = int(input("enter your age "))#Exception
-    if age<15:
-        print(f"(age) is miner")
-    elif age>=15:
-        print(f"(age) is Old")
-    else:
-        print("Enter a proper age")
-except:
-    print("please give number")
+while True:
+    try:
+        age = int(input("enter your age "))
+        if age<15:
+                    print(f"(age) is miner")
+        elif age>=15:
+                    print(f"(age) is Old")
+        else:
+                    print("Enter a proper age")
+    except:
+        print("please give number")
+    run=input("dp you want to contiune??(Y/N)\n")
+    if run=="n":
+        break
