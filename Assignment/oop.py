5196b14955c6976f8a3b23d8ea1c1512def7358f
VISHNU PAUDEL <vishnupaudel1989@gmail.com>
2025-06-17 07:41:29 +0545

    Encapsulation

--- Assignment/oop.py
+++ Assignment/oop.py
@@ -1,15 +1,21 @@
 class Teacher:#creating Class
     def __init__(self,name,age):#initialzing the attributes of class
-        self.name = name
-        self.age = age
-        
+        self.__name = name# making the attributes privates
+        self.__age = age
+    def set_name(self,name):
+            self._name= name
+            #using setter method to set tha value in private attributesclerxc
+    def  get_name(self):
+                return self.__name       
     def give_assignment(self):#Craeting the behaviour of a class
         print("complete the assignment")       
 Teacher1= Teacher("sunil",25)#creating object of the class
-#accessing the attributes of class
-print(Teacher1.name) 
-print(Teacher1.age)
+#accessing private attributes pf class is not allowed 
+#print(Teacher1.name) 
+#print(Teacher1.age) # not allowed
 #calling the function of the class
 Teacher1.give_assignment()
+Teacher1.set_name("janak")
+print(Teacher1.get_name())
       
     
