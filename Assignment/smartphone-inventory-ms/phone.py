33b942af4d6c485f375bdc59a36f7c7650f0ebb4
VISHNU PAUDEL <vishnupaudel1989@gmail.com>
2025-06-17 08:57:23 +0545

    smartphone-inventory-ms

---
+++ Assignment/smartphone-inventory-ms/phone.py
@@ -0,0 +1,22 @@
+#TODO creat a class name phone
+#withprivate attributes brand, model, modelnumber,price,storage
+#create setters and getters for the private attributes
+class Phone:
+    def __init__(self,model,brand,price,storage,quantity):
+        self.__model=model
+        self.__brand=brand
+        self.__price=price
+        self.__storage=storage
+        self.__quantity=quantity
+        
+    def set_model(self,model):
+        self._model= model
+        
+    def  get_model(self):
+        return self.__model 
+    
+    def set_brand(self.price)
+        self.__price= __price
+        return self._model
+    
+        
