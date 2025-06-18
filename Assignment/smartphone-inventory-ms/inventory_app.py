727b34b975302b016d6eece617408c433c19696b
VISHNU PAUDEL <vishnupaudel1989@gmail.com>
2025-06-18 08:38:14 +0545

    started with terminal user interface

--- Assignment/smartphone-inventory-ms/inventory_app.py
+++ Assignment/smartphone-inventory-ms/inventory_app.py
@@ -4,20 +4,38 @@ phone_inventory=[]
 
 #TODO add a phone in inventory
 #TODO ask the phone details from the user before adding the phone to inventory
-phone = Phone ("S20","samsung",5000,"32GB", "30")
-phone1 = Phone ("S63","samsung",8000,"32GB", "30")
+#TODO need to handle
+def add_phone():
+    model=str(input("Enter the phone model:"))
+    brand=str(input("Enter the phone brand:"))
+    price=int(input("Enter the phone price:"))
+    storage=str(input("Enter the phone storage:"))
+    quantity=int(input("Enter the phone quantity:"))
 
-phone_inventory.append(phone)
-phone_inventory.append(phone1)
-print(phone_inventory)
+    phone = Phone (model,brand,price,storage,quantity)
+
+    phone_inventory.append(phone)
+    print(phone_inventory)
 
 #TODO view details of phone
 #TODO how do i show information of the phone that the user wants.
-for phone in phone_inventory:
+def view_phone_details():
+    for phone in phone_inventory:
     
-    print(f"phone Model: {phone.get_model()}")
-    print(f"phone Brand: {phone.get_brand()}")
+        print(f"phone Model: {phone.get_model()}")
+        print(f"phone Brand: {phone.get_brand()}")
 #TODO update detail of phones
 #TODO delete a phone
-
 #TODO how to allow user to do the operations?
+#TODO how to let the user program as much as they want
+#TODO how to let user exit the program. 
+print("Enter 1 to add phone:")
+print("Enter 2 to view details:")
+option=int(input("choose and option"))
+if(option==1):
+    add_phone()
+elif(option==2):
+    view_phone_details()
+
+
+
--- Assignment/smartphone-inventory-ms/phone.py
+++ Assignment/smartphone-inventory-ms/phone.py
@@ -2,41 +2,41 @@
 #withprivate attributes brand, model, modelnumber,price,storage
 #create setters and getters for the private attributes
 class Phone:
-    def __init__(self,model,brand,price,storage,quantity):
+    def __init__(self,model:str,brand:str,price: int,storage:str,quantity:int):
         self.__model=model
         self.__brand=brand
         self.__price=price
         self.__storage=storage
         self.__quantity=quantity
         
-    def set_model(self,model):# Set for mobile Model
+    def set_model(self,model:str):# Set for mobile Model
         self.__model= model
         
-    def  get_model(self): #Get for mobile Model
+    def  get_model(self) -> str:  #Get for mobile Model
         return self.__model 
     
-    def set_brand(self,brand): # Set for mobile Brand
+    def set_brand(self,brand:str): # Set for mobile Brand
         self.__brand= brand
         
-    def get_brand(self):
+    def get_brand(self) ->int :
         return self.__brand #Get for mobile Brand
     
-    def set_price(self,price):# Set for mobile Price
+    def set_price(self,price:int):# Set for mobile Price
         self.__price= price
         
-    def get_price(self):
+    def get_price(self)->int:
         return self.__price  # Set for mobile Price
     
-    def set_storage(self,Storage):# Set for mobile storage
+    def set_storage(self,Storage:str):# Set for mobile storage
         self.__storage= Storage
         
-    def Set_storage(self):
+    def Set_storage(self)->str:
         return self.__storage  # Set for mobile storage
     
-    def set__quantity (self,quantity):# Set for mobile storage
+    def set__quantity (self,quantity:int):# Set for mobile storage
         self.__quantity= quantity
         
-    def Set_quantity(self):
+    def Set_quantity(self)->int:
         return self.__quantity  # Set for mobile storage
     
         
