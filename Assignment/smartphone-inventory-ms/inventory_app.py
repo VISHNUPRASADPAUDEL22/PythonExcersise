8309872fdd8575535350990997e7b258015a89d5
VISHNU PAUDEL <vishnupaudel1989@gmail.com>
2025-06-18 08:02:19 +0545

    view details of phone list

--- Assignment/smartphone-inventory-ms/inventory_app.py
+++ Assignment/smartphone-inventory-ms/inventory_app.py
@@ -5,12 +5,18 @@ phone_inventory=[]
 #TODO add a phone in inventory
 #TODO ask the phone details from the user before adding the phone to inventory
 phone = Phone ("S20","samsung",5000,"32GB", "30")
+phone1 = Phone ("S63","samsung",8000,"32GB", "30")
 
 phone_inventory.append(phone)
-
+phone_inventory.append(phone1)
 print(phone_inventory)
 
 #TODO view details of phone
+#TODO how do i show information of the phone that the user wants.
+for phone in phone_inventory:
+    
+    print(f"phone Model: {phone.get_model()}")
+    print(f"phone Brand: {phone.get_brand()}")
 #TODO update detail of phones
 #TODO delete a phone
 
