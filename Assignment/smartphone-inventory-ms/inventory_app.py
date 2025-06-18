24dbe124cae02c203a662ad7319b0d1c33305a77
VISHNU PAUDEL <vishnupaudel1989@gmail.com>
2025-06-18 07:53:16 +0545

    addphonein list

---
+++ Assignment/smartphone-inventory-ms/inventory_app.py
@@ -0,0 +1,17 @@
+from phone import Phone
+#TODO how do i implement my inventory?
+phone_inventory=[]
+
+#TODO add a phone in inventory
+#TODO ask the phone details from the user before adding the phone to inventory
+phone = Phone ("S20","samsung",5000,"32GB", "30")
+
+phone_inventory.append(phone)
+
+print(phone_inventory)
+
+#TODO view details of phone
+#TODO update detail of phones
+#TODO delete a phone
+
+#TODO how to allow user to do the operations?
--- Assignment/smartphone-inventory-ms/phone.py
+++ Assignment/smartphone-inventory-ms/phone.py
@@ -9,14 +9,34 @@ class Phone:
         self.__storage=storage
         self.__quantity=quantity
         
-    def set_model(self,model):
-        self._model= model
+    def set_model(self,model):# Set for mobile Model
+        self.__model= model
         
-    def  get_model(self):
+    def  get_model(self): #Get for mobile Model
         return self.__model 
     
-    def set_brand(self.price)
-        self.__price= __price
-        return self._model
+    def set_brand(self,brand): # Set for mobile Brand
+        self.__brand= brand
+        
+    def get_brand(self):
+        return self.__brand #Get for mobile Brand
+    
+    def set_price(self,price):# Set for mobile Price
+        self.__price= price
+        
+    def get_price(self):
+        return self.__price  # Set for mobile Price
+    
+    def set_storage(self,Storage):# Set for mobile storage
+        self.__storage= Storage
+        
+    def Set_storage(self):
+        return self.__storage  # Set for mobile storage
+    
+    def set__quantity (self,quantity):# Set for mobile storage
+        self.__quantity= quantity
+        
+    def Set_quantity(self):
+        return self.__quantity  # Set for mobile storage
     
         
